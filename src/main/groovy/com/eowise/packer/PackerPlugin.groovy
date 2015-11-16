64f47075342ef939771f05216c5b451f00a7de8e
aurel <aurelien.catinon@gmail.com>
2015-11-16 19:07:24 +0100

    Update to gdx-tools version 1.7.1

--- src/main/groovy/com/eowise/packer/PackerPlugin.groovy
+++ src/main/groovy/com/eowise/packer/PackerPlugin.groovy
@@ -7,7 +7,7 @@ class PackerPlugin implements Plugin<Project> {
 
     void apply(Project project) {
         project.configurations.create('tools')
-        project.dependencies.add('tools', 'com.badlogicgames.gdx:gdx-tools:1.5.3')
+        project.dependencies.add('tools', 'com.badlogicgames.gdx:gdx-tools:1.7.1')
         project.task("buildPacks")
         project.task("cleanPacks")
     }
